628a23b08d1f675e8066b3bcc0ef5448db49c27a
Asaf Kamber <kamberasaf@gmail.com>
2025-05-15 13:17:58 +0300

    Update test_animals.py

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,11 +1,11 @@
-from Aqua import Aqua
+from aqua import Aqua
 
 def test_add_animal():
     aquarium = Aqua(50, 30)
     result = aquarium.add_animal("TestFish", 5, 10, 10, 1, 0, 'sc')
-    assert result is True
+    assert result is True, "Failed to add a valid animal"
 
 def test_invalid_animal():
     aquarium = Aqua(50, 30)
     result = aquarium.add_animal("", -1, 0, 0, -1, -1, 'xx')
-    assert result is False
+    assert result is False, "Invalid animal was incorrectly added"
