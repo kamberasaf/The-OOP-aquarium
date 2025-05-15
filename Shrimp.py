b220630858c011ea9ce00a366d7fbfd33d98fd45
Asaf Kamber <kamberasaf@gmail.com>
2025-05-15 15:36:27 +0300

    Update and rename shrimp.py to Shrimp.py

--- Shrimp.py
+++ Shrimp.py
@@ -1,7 +1,7 @@
-from crab import Crab
+import Crab
 
 
-class Shrimp(Crab):
+class Shrimp(Crab.Crab):
     def __init__(self, name, age, x, y, directionH):
         super().__init__(name, age, x, y, directionH)
         self.width = 7
@@ -12,9 +12,9 @@ class Shrimp(Crab):
             '    * *',
             '****** ',
             '  * *  '
-        ]
+                ]
 
         if self.directionH == 0:
-            shrimp = [line[::-1] for line in shrimp]
+            shrimp = [i[::-1] for i in shrimp]  # a shrimp looking left.
 
         return shrimp
