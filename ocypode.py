20e0c88a126a5d9debd426017889a4315067022e
Asaf Kamber <kamberasaf@gmail.com>
2025-05-15 13:15:55 +0300

    Update and rename Ocypode.py to ocypode.py

--- ocypode.py
+++ ocypode.py
@@ -1,9 +1,10 @@
-import Crab
+# ocypode.py
+from crab import Crab
 
 
-class Ocypode(Crab.Crab):
-    def __init__(self, name, age, x, y, directionH):
-        super().__init__(name, age, x, y, directionH)
+class Ocypode(Crab):
+    def __init__(self, name, age, x, y, direction_h):
+        super().__init__(name, age, x, y, direction_h)
         self.width = 7
         self.height = 4
 
@@ -13,5 +14,5 @@ class Ocypode(Crab.Crab):
             '  ***  ',
             '*******',
             '*     *'
-                   ]
+        ]
         return ocypode
