8c882c1b871fbe3f64a9f0ed30ea737753a387db
Asaf Kamber <kamberasaf@gmail.com>
2025-05-15 13:18:58 +0300

    Update add_animals_example.py

--- examples/add_animals_example.py
+++ examples/add_animals_example.py
@@ -1,9 +1,15 @@
-from Aqua import Aqua
+from aqua import Aqua
 
 def run_example():
     myaqua = Aqua(50, 30)
-    myaqua.add_animal("scalarfish1", 4, 10, 10, 1, 0, 'sc')
-    myaqua.add_animal("molyfish2", 12, 35, 15, 0, 1, 'mo')
+    
+    success1 = myaqua.add_animal("scalarfish1", 4, 10, 10, 1, 0, 'sc')
+    print(f"Adding scalarfish1 successful? {success1}")
+    
+    success2 = myaqua.add_animal("molyfish2", 12, 35, 15, 0, 1, 'mo')
+    print(f"Adding molyfish2 successful? {success2}")
+    
+    print("\nAquarium board:")
     myaqua.print_board()
 
 if __name__ == "__main__":
